edba534b6bdcbacbf3e9bff40d3a9e7ef40959eb
TBusen <busen@me.com>
2017-02-20 22:19:29 -0600

    working template, had to remove comments from html template

--- app/views.py
+++ app/views.py
@@ -1,22 +1,16 @@
 #The views are the handlers that respond to requests from web browsers or other clients.
 #In Flask handlers are written as Python functions. Each view function is mapped to one or more request URLs.
 
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname':'Miguel'} # fake user
-    return '''
-<html>
-    <head>
-        <title>Home Page</title>
-    </head>
-    <body>
-    <h1>Hello,''' + user['nickname'] + '''</h1>
-    </body>
-</html>
-'''
+    user = {'nickname': 'Miguel'}  # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
 
 # This view returns a string to be displayed on the client's browser
 

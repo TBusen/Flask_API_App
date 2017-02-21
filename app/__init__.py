4d1f70a3f409d84cee964e965e729f9bd158d2c3
TBusen <busen@me.com>
2017-02-20 21:36:18 -0600

    changed to comments

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 from app import views
 
-'''The above script creates the appliation object (of class Flask)
-and then imports the views module.  Do not confuse app the variable
-which gets assigned the Flask instance with app the package from which
-we import the views module'''
+#The above script creates the appliation object (of class Flask)
+#and then imports the views module.  Do not confuse app the variable
+#which gets assigned the Flask instance with app the package from which
+#we import the views module

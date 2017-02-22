0c654b20c9ed9a6246f5353285485ce2d978b7a1
TBusen <busen@me.com>
2017-02-21 21:57:18 -0600

    add for loops to template to allow for usernames and comments.  Template must be dynamic in handeling number of comments from the users

--- app/views.py
+++ app/views.py
@@ -8,9 +8,24 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname': 'Miguel'}  # fake user
+    posts = [# fake list of posts
+        {
+            'author':{'nickname':'John'},
+            'body':'Beautiful day in Portland!'
+            },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+            },
+        {
+            'author':{'nickname':'John'},
+            'body':'Beautiful day in Peoria!'
+            }
+        ]
     return render_template('index.html',
-                           #title='Home',
-                           user=user)
+                           title='Home',
+                           user=user,
+                           posts=posts)
 
 # This view returns a string to be displayed on the client's browser
 
